ed3b0ebd26b5fdfa93a8d1865d6c7a9c0f19cdc8
Admin <kqadb@mail.ru>
2023-01-20 02:58:25 +0500

    я немного не поняла, все "галочки" нужно ставить, или тоько на классах и пакетах? на всякий случай отправила все

---
+++ src/main/java/CalculatorApplication.java
@@ -0,0 +1,14 @@
+import com.sky.pro.calculate_application.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+
+
+
+
+    }
+}
---
+++ src/main/java/com/sky/pro/calculate_application/SpringBootApplication.java
@@ -0,0 +1,4 @@
+package com.sky.pro.calculate_application;
+
+public @interface SpringBootApplication {
+}
